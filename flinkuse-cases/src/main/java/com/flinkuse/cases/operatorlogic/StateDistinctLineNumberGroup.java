4f93b1ae1c9b2a4814270e7a00c07622e47aa7cc
learn <learn_lian@juzhenpei.com>
2024-01-26 16:52:30 +0800

    case 利用状态去重设置行号,按行号聚合

--- flinkuse-cases/src/main/java/com/flinkuse/cases/operatorlogic/StateDistinctLineNumberGroup.java
+++ flinkuse-cases/src/main/java/com/flinkuse/cases/operatorlogic/StateDistinctLineNumberGroup.java
@@ -1,6 +1,9 @@
 package com.flinkuse.cases.operatorlogic;
 
 import com.flinkuse.core.base.StreamApp;
+import org.apache.flink.api.common.RuntimeExecutionMode;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.RichReduceFunction;
 import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.api.java.tuple.Tuple2;
@@ -8,8 +11,6 @@ import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
-import org.apache.flink.streaming.api.functions.windowing.RichWindowFunction;
-import org.apache.flink.streaming.api.windowing.windows.GlobalWindow;
 import org.apache.flink.util.Collector;
 
 import java.util.ArrayList;
@@ -19,22 +20,23 @@ import java.util.List;
  * @author learn
  * @date 2024/1/22 15:27
  */
-public class StateDistinctLineNumber extends StreamApp {
+public class StateDistinctLineNumberGroup extends StreamApp {
 
     private final Integer skuNumber;
 
-    public StateDistinctLineNumber(String[] args, String jobName) {
+    public StateDistinctLineNumberGroup(String[] args, String jobName) {
         super(args, jobName);
         skuNumber = getScpsParams().getInt("bosch_querysku_nmber", 5);
     }
 
     public static void main(String[] args) throws Exception {
-        new StateDistinctLineNumber(args, "StateDistinctLineNumber").start();
+        new StateDistinctLineNumberGroup(args, "StateDistinctLineNumber").start();
     }
     @Override
     public void run(StreamExecutionEnvironment streamEnv) {
         try {
-            // streamEnv.setRuntimeMode(RuntimeExecutionMode.BATCH);
+            // reduce算子不同模式有不同结果
+            streamEnv.setRuntimeMode(RuntimeExecutionMode.BATCH);
 
             prodGroupLogic(streamEnv.fromElements("3397016430","3397016432","0092S47328","222","3333","44444")).print();
 
@@ -43,6 +45,11 @@ public class StateDistinctLineNumber extends StreamApp {
         }
     }
 
+    /**
+     * 利用状态去重设置行号,按行号聚合
+     * @param inputStream
+     * @return
+     */
     private DataStream<List<String>> prodGroupLogic(DataStream<String> inputStream) {
 
         return inputStream.keyBy(value -> value
@@ -70,7 +77,7 @@ public class StateDistinctLineNumber extends StreamApp {
                 }
             }
         }).keyBy(k -> k.f1
-        ).process(new KeyedProcessFunction<Integer, Tuple2<String, Integer>, Tuple2<String, Long>>() {
+        ).process(new KeyedProcessFunction<Integer, Tuple2<String, Integer>, Tuple2<List<String>, Long>>() {
             /*
              状态编程
              给每个元素设置行号
@@ -83,26 +90,29 @@ public class StateDistinctLineNumber extends StreamApp {
                 counterState = getRuntimeContext().getState(stateDescriptor);
             }
             @Override
-            public void processElement(Tuple2<String, Integer> value, Context ctx, Collector<Tuple2<String, Long>> out) throws Exception {
+            public void processElement(Tuple2<String, Integer> value, Context ctx, Collector<Tuple2<List<String>, Long>> out) throws Exception {
                 Long currentCount = counterState.value();
                 if (currentCount == null) {
                     currentCount = 0L;
                 }
                 // 更新计数器
                 counterState.update(currentCount + 1);
-                Tuple2<String, Long> t = Tuple2.of(value.f0, currentCount / skuNumber);
+                List<String> t1 = new ArrayList<>();
+                t1.add(value.f0);
+                Tuple2<List<String>, Long> t = Tuple2.of(t1, currentCount / skuNumber);
                 out.collect(t);
             }
         }).keyBy(f -> f.f1
-        ).countWindow(5
-        ).apply(new RichWindowFunction<Tuple2<String, Long>, List<String>, Long, GlobalWindow>() {
+        ).reduce(new RichReduceFunction<Tuple2<List<String>, Long>>() {
             @Override
-            public void apply(Long aLong, GlobalWindow window, Iterable<Tuple2<String, Long>> input, Collector<List<String>> out) throws Exception {
-                List<String> res = new ArrayList<>();
-                for (Tuple2<String, Long> t2 : input) {
-                    res.add(t2.f0);
-                }
-                out.collect(res);
+            public Tuple2<List<String>, Long> reduce(Tuple2<List<String>, Long> v1, Tuple2<List<String>, Long> v2) throws Exception {
+                v1.f0.addAll(v2.f0);
+                return v1;
+            }
+        }).map(new MapFunction<Tuple2<List<String>, Long>, List<String>>() {
+            @Override
+            public List<String> map(Tuple2<List<String>, Long> value) throws Exception {
+                return value.f0;
             }
         });
     }
